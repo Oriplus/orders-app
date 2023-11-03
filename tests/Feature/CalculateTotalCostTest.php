e477b4bf74c8c074b66b90b964055c6800da6bef
Oriana Morillo <orianacmv@gmail.com>
2023-11-03 17:10:02 -0300

    #9 test(TotalCostJob): add test for totalCostJob

---
+++ tests/Feature/CalculateTotalCostTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Jobs\CalculateTotalCost;
+use App\Models\Order;
+use App\Models\OrderLine;
+use App\Models\Product;
+use Illuminate\Support\Facades\Queue;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Log;
+use Tests\TestCase;
+
+class CalculateTotalCostTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $order = Order::factory()->create();
+
+        $product = Product::factory()->create([
+            'cost' => 10.00
+        ]);
+
+        OrderLine::factory()->create([
+            'qty' => 2,
+            'product_id' => $product->id,
+            'order_id' => $order->id
+        ]);
+    }
+
+    public function shouldDispatchesCalculateTotalCostJob(): void
+    {
+        Queue::fake();
+        CalculateTotalCost::dispatch();
+        Queue::assertPushed(CalculateTotalCost::class);
+    }
+
+    public function shouldCalculatesTotalCostCorrectly(): void
+    {
+        Log::shouldReceive('info')
+            ->once()
+            ->withArgs(function ($message) {
+                return str_contains($message, 'Orders Cost Total: 20');
+            });
+        $job = new CalculateTotalCost();
+        $job->handle();
+    }
+}
